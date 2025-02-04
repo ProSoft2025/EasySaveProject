356e66794d88117b51400ae8619bd758b759f698
Jérémy DUPRÉ <jeremy.dupre@viacesi.fr>
2025-02-04 10:55:57 +0100

    Add constructor and comments

--- EasySaveV1/LogEntry.cs
+++ EasySaveV1/LogEntry.cs
@@ -1,4 +1,7 @@
-﻿namespace EasySave
+﻿//The LogEntry class contains the details of the backup actions
+
+
+namespace EasySave
 {
     public class LogEntry
     {
@@ -8,5 +11,17 @@
         public string TargetPath { get; set; }
         public int FileSize { get; set; }
         public int TransferTime { get; set; }
+    
+        //constructor
+        public LogEntry(string taskName, string sourcePath, string targetPath, int fileSize, int transferTime)
+        {
+            Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            TaskName = taskName;
+            SourcePath = sourcePath;
+            TargetPath = targetPath;
+            FileSize = fileSize;
+            TransferTime = transferTime;
+        }
     }
+
 }
