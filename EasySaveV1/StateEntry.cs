fde2a4a39e907ba49673d9fee6f045da0144b523
Jérémy DUPRÉ <jeremy.dupre@viacesi.fr>
2025-02-06 11:47:27 +0100

    Modif pour stateManager

--- EasySaveV1/StateEntry.cs
+++ EasySaveV1/StateEntry.cs
@@ -1,21 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+/* Summary */
 
 namespace EasySave
 {
     public class StateEntry
     {
         public string TaskName { get; set; }
-        public string Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
         public string Status { get; set; }
         public int TotalFiles { get; set; }
-        public int TotalSize { get; set; }
+        public long TotalSize { get; set; }
         public int Progress { get; set; }
         public int RemainingFiles { get; set; }
-        public int RemainingSize { get; set; }
+        public long RemainingSize { get; set; }
         public string CurrentSource { get; set; }
         public string CurrentTarget { get; set; }
     }
