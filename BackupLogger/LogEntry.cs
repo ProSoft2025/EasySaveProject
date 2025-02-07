0b37ab7ed5f57b42d33b2c3d6d0adeee25a075d3
matheo PENTEADO <matheo.penteado@viacesi.fr>
2025-02-07 09:04:15 +0100

    ajout de logentry

---
+++ BackupLogger/LogEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BackupLogger
+{
+    public class LogEntry
+    {
+        public string Timestamp { get; set; }
+        public string TaskName { get; set; }
+        public string SourcePath { get; set; }
+        public string TargetPath { get; set; }
+        public int FileSize { get; set; }
+        public int TransferTime { get; set; }
+
+        //constructor
+        public LogEntry(string taskName, string sourcePath, string targetPath, int fileSize, int transferTime)
+        {
+            Timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            TaskName = taskName;
+            SourcePath = sourcePath;
+            TargetPath = targetPath;
+            FileSize = fileSize;
+            TransferTime = transferTime;
+        }
+    }
+
+}
