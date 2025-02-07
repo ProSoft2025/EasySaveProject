c2c7594f26a31322b89da1d5fb865a6f54c1dcde
matheo PENTEADO <matheo.penteado@viacesi.fr>
2025-02-07 09:06:37 +0100

    stash before pull

--- BackupLogger/obj/Debug/net8.0/BackupLogger.AssemblyInfo.cs
+++ BackupLogger/obj/Debug/net8.0/BackupLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackupLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeda367bd6bf10c041e05586b2fd8fcac4b7061a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968039adc255a25f38c189dd731a40b3ad23df71")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackupLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackupLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EasySaveV1/UserInterface.cs
+++ EasySaveV1/UserInterface.cs
@@ -201,6 +201,6 @@ namespace EasySave {
         }
     }
 
-   }
+ }
 
 
