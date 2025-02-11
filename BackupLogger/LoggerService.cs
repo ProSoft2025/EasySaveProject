93d4082b8a09e01f2e163c591f657cda8177173b
Matheo <matheo.penteado@viacesi.fr>
2025-02-11 11:21:42 +0100

    Loggerservice dans dll

---
+++ BackupLogger/LoggerService.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+/*"The Logger class records the save actions of the LogEntry class
+ * in a JSON file with line breaks for easy reading */
+
+namespace BackupLogger
+{
+    public class LoggerService
+    {
+        private Logger _backupLogger;
+
+        public LoggerService(string logFilePath)
+        {
+            _backupLogger = new Logger(logFilePath);
+        }
+        public Logger GetBackupLogger()
+        {
+            return _backupLogger;
+        }
+        public void LogBackupCreation(string filelog)
+        {
+            if (!File.Exists(filelog))
+            {
+                // Créer un fichier vide
+                File.WriteAllText(filelog, "[]");
+            }
+        }
+    }
+}
+
